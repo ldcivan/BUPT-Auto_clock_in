33918b2b9ac9aa865a8de208dd4aeb79e5cf54d2
Yujio_Nako <69411867+ldcivan@users.noreply.github.com>
2022-10-16 00:41:23 +0800

    支持多人，隐藏运行和日志功能了

--- auto_sign.py
+++ auto_sign.py
@@ -4,32 +4,18 @@ import time
 import sys
 from interval import Interval
 
+
 def write(data):
     File = open('log.txt', mode='a',encoding="utf-8")
     File.write(data + '\n')
     File.close()
 
-def daka():
-    write("读取config")
-    print("正在读取config.txt")
-    with open('./config.txt', 'r') as file:
-        l = file.readlines()  # 按行读取TXT文件，都是字符串类型
-        d = {}
-        for i in l:
-            s = i.replace('\n', '')  # 去除换行符
-            s0 = s.split(sep=':')  # 以:分割字符串，左边是键，右边是值。同样都是字符串类型
-            if '"' in s0[1]:
-                s0[1] = s0[1].replace('"', '')  # 字符串存在双引号，说明原本的值就是字符串类型。去掉多余的双引号
-            else:
-                s0[1] = int(s0[1])  # 说明原本值是整型，强制类型转换
-            d[s0[0]] = s0[1]  # 键值对添加到字典中
 
-    user = d['username']
-    pw = d['pw']
-    hidden = d['hidden']
-
+def daka(user, pw, hidden):
     global window
-
+    print(user)
+    print(pw)
+    print(hidden)
     if hidden == 1:
         write("使用后台模式")
         print("您决定使用后台模式运行")
@@ -60,7 +46,7 @@ def daka():
         print("无法自动登录，正在尝试使用学号及密码登录")
         try:
             time.sleep(3)
-            driver.execute_script("var username ='" + user + "';var password ='" + pw + "';localStorage.loginTypeUsername = 'password';if (username && password) {parent.doLogin( username, password, 'username_password');}")
+            driver.execute_script("var username =" + user + ";var password =" + pw + ";localStorage.loginTypeUsername = 'password';if (username && password) {parent.doLogin( username, password, 'username_password');}")
         except:
             write("在登陆页面出错，可能是账号密码错误")
             print("在登陆页面出错，可能是账号密码错误")
@@ -75,11 +61,11 @@ def daka():
             print("登陆成功")
             try:
                 driver.execute_script("this.vm.hasFlag = 0;this.vm.info = this.vm.oldInfo;this.vm.info.created = Number(Date.parse( new Date() ).toString().substr(0,10));var now = new Date();var year = now.getFullYear().toString();var month = now.getMonth().toString();var date = now.getDate().toString();if (month < 10) month = '0' + month;if (date < 10) date = '0' + date;this.vm.info.date = Number(year + month + date);this.vm.save();")
-                write("打卡成功")
-                print("打卡成功，如果是首次使用，建议人工查看是否已打卡")
+                write(user + "打卡成功")
+                print(user + "打卡成功，如果是首次使用，建议人工查看是否已打卡")
             except:
-                write("打卡失败")
-                print("打卡失败，请手动打卡")
+                write(user + "打卡失败")
+                print(user + "打卡失败，请手动打卡")
         else:
             write("登陆失败，可能是浏览器未正确跳转")
             print("登陆失败，可能是浏览器未正确跳转")
@@ -95,6 +81,27 @@ def daka():
     print("浏览器已关闭")
 
 
+write("读取config")
+print("正在读取config.txt")
+with open('./config.txt', 'r') as file:
+    l = file.readlines()  # 按行读取TXT文件，都是字符串类型
+    d = {}
+    for i in l:
+        s = i.replace('\n', '')  # 去除换行符
+        s0 = s.split(sep=':')  # 以:分割字符串，左边是键，右边是值。同样都是字符串类型
+        if '"' in s0[1]:
+            s0[1] = s0[1].replace('"', '')  # 字符串存在双引号，说明原本的值就是字符串类型。去掉多余的双引号
+        else:
+            s0[1] = int(s0[1])  # 说明原本值是整型，强制类型转换
+        d[s0[0]] = s0[1]  # 键值对添加到字典中
+
+
+user = d['username']
+user = user.split(",")
+pw = d['pw']
+pw = pw.split(",")
+hidden = d['hidden']
+
 while True:
     # 当前时间
     now_localtime = time.strftime("%H:%M:%S", time.localtime())
@@ -104,9 +111,12 @@ while True:
     time_interval = Interval("00:02:00", "00:03:00")
 
     if now_time in time_interval:
+        with open("./log.txt", 'w') as file:
+            file.truncate(0)
         write(str(now_time) + "开始打卡")
         print("开始打卡")
-        daka()
+        for i in range(len(user)):
+            daka(user[i], pw[i], hidden)
         time.sleep(120)
     else:
         write(str(now_time) + "  等待")
