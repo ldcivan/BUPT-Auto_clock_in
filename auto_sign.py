aba8695a6e450fc35fab0b90a13d60945bbdd3df
Yujio_Nako <69411867+ldcivan@users.noreply.github.com>
2022-10-15 23:28:00 +0800

    Add files via upload

--- auto_sign.py
+++ auto_sign.py
@@ -4,7 +4,13 @@ import time
 import sys
 from interval import Interval
 
+def write(data):
+    File = open('log.txt', mode='a',encoding="utf-8")
+    File.write(data + '\n')
+    File.close()
+
 def daka():
+    write("读取config")
     print("正在读取config.txt")
     with open('./config.txt', 'r') as file:
         l = file.readlines()  # 按行读取TXT文件，都是字符串类型
@@ -25,30 +31,38 @@ def daka():
     global window
 
     if hidden == 1:
+        write("使用后台模式")
         print("您决定使用后台模式运行")
         try:
             window = Display(visible=False, size=(800, 600))
+            write("正在启动虚拟窗口")
             print("正在启动虚拟窗口")
             window.start()
+            write("虚拟窗口已开启")
             print("虚拟窗口已开启")
         except:
+            write("不支持后台模式，已回归默认")
             print("您的电脑不支持后台模式，接下来将以前台模式运行\n无需担心，若一切正常，前台浏览器会自动关闭")
             hidden = 0
 
+    write("正在启动浏览器")
     print("正在启动浏览器")
     driver = webdriver.Firefox()
     driver.set_window_position(x=-10000, y=-10000)
-    print("浏览器已启动正在打开网页，目标app.bupt.edu.cn")
+    write("浏览器已启动，正在打开目标")
+    print("浏览器已启动，正在打开网页，目标app.bupt.edu.cn")
     driver.get("https://app.bupt.edu.cn/ncov/wap/default/index")
 
     try:
         driver.execute_script("this.vm.hasFlag = 0;this.vm.info = this.vm.oldInfo;this.vm.info.created = Number(Date.parse( new Date() ).toString().substr(0,10));var now = new Date();var year = now.getFullYear().toString();var month = now.getMonth().toString();var date = now.getDate().toString();if (month < 10) month = '0' + month;if (date < 10) date = '0' + date;this.vm.info.date = Number(year + month + date);this.vm.save();")
     except:
+        write("无法自动登录，正在尝试使用学号及密码登录")
         print("无法自动登录，正在尝试使用学号及密码登录")
         try:
             time.sleep(3)
             driver.execute_script("var username ='" + user + "';var password ='" + pw + "';localStorage.loginTypeUsername = 'password';if (username && password) {parent.doLogin( username, password, 'username_password');}")
         except:
+            write("在登陆页面出错，可能是账号密码错误")
             print("在登陆页面出错，可能是账号密码错误")
             driver.quit()
             if hidden == 1:
@@ -57,21 +71,27 @@ def daka():
 
         time.sleep(3)
         if driver.current_url == "https://app.bupt.edu.cn/ncov/wap/default/index":
+            write("登陆成功")
             print("登陆成功")
             try:
                 driver.execute_script("this.vm.hasFlag = 0;this.vm.info = this.vm.oldInfo;this.vm.info.created = Number(Date.parse( new Date() ).toString().substr(0,10));var now = new Date();var year = now.getFullYear().toString();var month = now.getMonth().toString();var date = now.getDate().toString();if (month < 10) month = '0' + month;if (date < 10) date = '0' + date;this.vm.info.date = Number(year + month + date);this.vm.save();")
+                write("打卡成功")
                 print("打卡成功，如果是首次使用，建议人工查看是否已打卡")
             except:
+                write("打卡失败")
                 print("打卡失败，请手动打卡")
         else:
+            write("登陆失败，可能是浏览器未正确跳转")
             print("登陆失败，可能是浏览器未正确跳转")
 
     time.sleep(2)
 
     if hidden == 1:
         window.stop()
+        write("虚拟窗口已关闭")
         print("虚拟窗口已关闭")
     driver.quit()
+    write("浏览器已关闭")
     print("浏览器已关闭")
 
 
@@ -84,9 +104,11 @@ while True:
     time_interval = Interval("00:02:00", "00:03:00")
 
     if now_time in time_interval:
+        write(str(now_time) + "开始打卡")
         print("开始打卡")
         daka()
         time.sleep(120)
     else:
+        write(str(now_time) + "  等待")
         print("等待")
         time.sleep(40)
